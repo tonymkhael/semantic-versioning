b68a8ac1d27a58054b1891e676200d40db1c4c9f
MKHAEL Tony <Tony.MKHAEL@murex.com>
2016-07-04 16:32:09 +0200

    remove hprof (+2 squashed commit)
    Squashed commit:
    
    [9846506] First checkpoint in documentation
    
    [f22172f] amend

---
+++ src/main/java/murex/cl/example/SemanticVersioning.java
@@ -0,0 +1,19 @@
+package murex.cl.example;
+
+public final class SemanticVersioning{
+
+  //tag::anchor[]
+
+  /**
+   * A sample for the javadoc
+   * @since 0.0.1
+   * @param args Tha arguments to this magnificient application
+   */
+  public static void main(String[] args){
+    System.out.println("Hello Semantic versioning");
+  }
+
+  //end::anchor[]
+
+}
+
